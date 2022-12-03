e0ccb153789b4cc3b18f9d8851e3672844850501
Nam Dang Phuong <nam.dangphuong26@gmail.com>
2022-12-03 11:22:42 +0700

    [dev]: Update
    - New Route: /pin/:id: get a pin

---
+++ src/pin/pin.controller.ts
@@ -0,0 +1,31 @@
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiOkResponse, ApiParam, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/guards';
+import { ApiCommon } from 'src/decorators/common-api.docs';
+import { PinService } from './pin.service';
+import { GetPinOutput } from './swagger/output/get-pin.output';
+
+@ApiTags('pin')
+@Controller('pin')
+export class PinController {
+  constructor(private pinService: PinService) {}
+
+  @ApiCommon()
+  @ApiOkResponse({ type: GetPinOutput })
+  @ApiParam({
+    name: 'id',
+    type: 'integer',
+    required: true,
+  })
+  @Get(':id')
+  @UseGuards(JwtAuthGuard)
+  async getPin(@Param('id', new ParseIntPipe()) id: number) {
+    return await this.pinService.getPin(id);
+  }
+}
